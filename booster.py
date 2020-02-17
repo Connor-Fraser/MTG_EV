8e66949597693eb43006f35a079d4f01dfb24c10
Connor <connor.fraser4@gmail.com>
2020-02-17 17:11:44 -0500

    Create booster.py file with some constants

---
+++ booster.py
@@ -0,0 +1,7 @@
+#These values are accuarte for Shards of Alara onwards but some sets have special rules not
+#currently accounted for https://mtg.gamepedia.com/Booster_pack
+
+FOIL_CHANCE = 1/8 #Can be any card in the set
+PACK_COMMONS = 10
+PACK_UNCOMMONS = 3
+PACK_RARE = 1
